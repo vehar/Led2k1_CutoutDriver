45a094851d1bb23d872bfdb563061938557a16b6
vehar <vladdxmel@gmail.com>
2018-05-04 22:46:50 +0300

    change gerbers to jlcpcb rules

---
+++ GarbageCleanerAltiumGerberdFabrication.bat
@@ -0,0 +1,14 @@
+del "*.bak" /f /s /a: H
+
+::Altium
+del "*.drr" /f /s /a: H
+del "*.ldp" /f /s /a: H
+del "Status Report.txt" /f /s /a: H
+del "*.extrep" /f /s /a: H
+del "*.rep" /f /s /a: H
+del "*.APR_LIB" /f /s /a: H
+
+sleep 90
+
+
+
